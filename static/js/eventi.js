56367e2f6a136a9cdf1d77c343ffe6f9f5ab11ca
Daniele Pala <pala.daniele@gmail.com>
2017-06-04 10:22:32 +0200

    responsive calendar for very small screens

--- static/js/eventi.js
+++ static/js/eventi.js
@@ -2,7 +2,7 @@ $(function() {
     var calendar = $("#calendario-eventi").calendar(
 	{
 	    tmpl_path: "/tmpls/",
-	    events_source: "{{ $.Site.BaseURL }}data/eventi.json",
+	    events_source: "/data/eventi.json",
 	    view: 'month',
 	    language: 'it-IT',
 	    weekbox: false,
@@ -10,12 +10,49 @@ $(function() {
 		$('#mese-eventi').text(this.getTitle());
 	    }
 	});
-
+    var mediaQueryList = window.matchMedia("(max-width: 550px)");
+    mediaQueryList.addListener(handleSizeChange);
     $('.btn-group button[data-calendar-nav]').each(function() {
 	var $this = $(this);
 	$this.click(function() {
 	    calendar.navigate($this.data('calendar-nav'));
+	    handleSizeChange();
 	});
     });
     calendar.navigate("today");
+    function handleSizeChange(evt) {
+	if (mediaQueryList.matches) {
+	    $(".cal-row-head > .cal-cell1").text(function(i, oldText) {
+		return oldText[0];
+	    });
+	} else {
+	    $(".cal-row-head > .cal-cell1").text(function(i, oldText) {
+		var ret = "";
+		switch (i) {
+		case 0:
+		    ret = "Lunedì";
+		    break;
+		case 1:
+		    ret = "Martedì";
+		    break;
+		case 2:
+		    ret = "Mercoledì";
+		    break;
+		case 3:
+		    ret = "Giovedì";
+		    break;
+		case 4:
+		    ret = "Venerdì";
+		    break;
+		case 5:
+		    ret = "Sabato";
+		    break
+		case 6:
+		    ret = "Domenica";
+		    break;
+		}
+		return ret;
+	    });
+	}
+    };
 });
